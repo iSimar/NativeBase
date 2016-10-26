a0e2d13a66a1ce913b7111e7b6299d96b365e51f
Simar <hello@simarsingh.com>
2016-10-25 23:55:55 -0400

    Update Header.js

--- Components/Widgets/Header.js
+++ Components/Widgets/Header.js
@@ -160,7 +160,7 @@ export default class Header extends NativeBaseComponent {
                         newChildren.push(<View key='btn1' style={{flex: 1, alignItems: 'center', justifyContent: 'flex-start', flexDirection: 'row', marginLeft: -10, marginRight: 12}}>
                         {React.cloneElement(buttons[0], {style: this.getInitialStyle().toolbarButton, header : true, textStyle: {color: this.getTheme().toolbarTextColor}})}
                         </View>)
-                        newChildren.push(<View key='title' style={{flex: 3, alignSelf: 'stretch', alignItems: 'center', justifyContent: 'center'}}>
+                        newChildren.push(<View key='title' style={{flex: 4, alignSelf: 'stretch', alignItems: 'center', justifyContent: 'center'}}>
                         {[title[0]]}
                         </View>)
                         for (let i = 1; i < buttons.length; i++) {
