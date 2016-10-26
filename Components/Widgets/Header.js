9bd68ffee93780dba87b6c7a240da90ebc38c48a
Simar <hello@simarsingh.com>
2016-10-25 23:44:56 -0400

    Update Header.js

--- Components/Widgets/Header.js
+++ Components/Widgets/Header.js
@@ -157,14 +157,14 @@ export default class Header extends NativeBaseComponent {
                         }
                     }
                     else {
-                        newChildren.push(<View key='btn1' style={{alignItems: 'center', justifyContent: 'flex-start', flexDirection: 'row', marginLeft: -10, marginRight: 12}}>
+                        newChildren.push(<View key='btn1' style={{flex: 1, alignItems: 'center', justifyContent: 'flex-start', flexDirection: 'row', marginLeft: -10, marginRight: 12}}>
                         {React.cloneElement(buttons[0], {style: this.getInitialStyle().toolbarButton, header : true, textStyle: {color: this.getTheme().toolbarTextColor}})}
                         </View>)
                         newChildren.push(<View key='title' style={{flex: 3, alignSelf: 'stretch', alignItems: 'center', justifyContent: 'center'}}>
                         {[title[0]]}
                         </View>)
                         for (let i = 1; i < buttons.length; i++) {
-                            newChildren.push(<View key={'btn' + (i+1)} style={{alignItems: 'center', justifyContent: 'flex-start', flexDirection: 'row', marginRight: -7}}>
+                            newChildren.push(<View key={'btn' + (i+1)} style={{flex: 1, alignItems: 'center', justifyContent: 'flex-end', flexDirection: 'row', marginRight: -7}}>
                             {React.cloneElement(buttons[i], {style: this.getInitialStyle().toolbarButton, header : true, textStyle: {color: this.getTheme().toolbarTextColor}})}
                             </View>)
                         }
